b03ec1e65e7b75c6a97c5e9555a7dfbc58639b2d
Ignacio1996 <Ignacio1996>
2023-08-22 22:00:43 +0000

    Pushed from Locofy Builder

--- components/feed.js
+++ components/feed.js
@@ -6,30 +6,143 @@ const Feed = () => {
     <div className={styles.feed}>
       <ThreadInput />
       <Thread
-        likeCount="7 Likes"
-        showReactions
         threadContentText="I’ve been exploring ways of setting up variables in Figma if you have two different sets of global colours for light and dark themes with multiple brands. If you want to learn more about it, DM me, please"
+        showLikes
+        username="aura"
+        timeSpent="2min"
       />
-      <Thread
-        likeCount="2 Likes"
-        showReactions={false}
-        threadContentText={`guys, just see that now we have a "Save for Later" I've been waiting for this so much`}
-      />
-      <Thread
-        likeCount="16 Likes"
-        showReactions
-        threadContentText="Hold onto your seats, folks! The new iPhone is coming in hot, ready to rock our world! Imagine the stunning photos and videos I'll capture with this beauty! 📸💥 #BringItOnNewiPhone #BeyondExcited"
-      />
-      <Thread
-        likeCount="2 Likes"
-        showReactions={false}
-        threadContentText={`guys, just see that now we have a "Save for Later" I've been waiting for this so much`}
-      />
-      <Thread
-        likeCount="16 Likes"
-        showReactions
-        threadContentText="Hold onto your seats, folks! The new iPhone is coming in hot, ready to rock our world! Imagine the stunning photos and videos I'll capture with this beauty! 📸💥 #BringItOnNewiPhone #BeyondExcited"
-      />
+      <div className={styles.thread}>
+        <div className={styles.thread1}>
+          <div className={styles.thread2}>
+            <img className={styles.avatarIcon} alt="" src="/avatar2.svg" />
+          </div>
+          <div className={styles.infosActions}>
+            <div className={styles.info}>
+              <div className={styles.headingInfos}>
+                <div className={styles.aura}>aura</div>
+                <div className={styles.rightInfos}>
+                  <div className={styles.min}>2min</div>
+                  <div className={styles.dots}>
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.iveBeenExploring}>
+                guys, just see that now we have a "Save for Later" I've been
+                waiting for this so much
+              </div>
+            </div>
+            <div className={styles.actions}>
+              <img className={styles.likeIcon} alt="" src="/like3.svg" />
+            </div>
+            <div className={styles.reactions}>
+              <div className={styles.likes}>2 Likes</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.thread}>
+        <div className={styles.thread1}>
+          <div className={styles.thread2}>
+            <img className={styles.avatarIcon} alt="" src="/avatar3.svg" />
+          </div>
+          <div className={styles.infosActions}>
+            <div className={styles.info}>
+              <div className={styles.headingInfos}>
+                <div className={styles.aura}>aura</div>
+                <div className={styles.rightInfos}>
+                  <div className={styles.min}>2min</div>
+                  <div className={styles.dots}>
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.iveBeenExploring}>
+                Hold onto your seats, folks! The new iPhone is coming in hot,
+                ready to rock our world! Imagine the stunning photos and videos
+                I'll capture with this beauty! 📸💥 #BringItOnNewiPhone
+                #BeyondExcited
+              </div>
+            </div>
+            <div className={styles.actions}>
+              <img className={styles.likeIcon} alt="" src="/like3.svg" />
+            </div>
+            <div className={styles.reactions1}>
+              <div className={styles.likes}>16 Likes</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.thread}>
+        <div className={styles.thread1}>
+          <div className={styles.thread2}>
+            <img className={styles.avatarIcon} alt="" src="/avatar4.svg" />
+          </div>
+          <div className={styles.infosActions}>
+            <div className={styles.info}>
+              <div className={styles.headingInfos}>
+                <div className={styles.aura}>aura</div>
+                <div className={styles.rightInfos}>
+                  <div className={styles.min}>2min</div>
+                  <div className={styles.dots}>
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.iveBeenExploring}>
+                guys, just see that now we have a "Save for Later" I've been
+                waiting for this so much
+              </div>
+            </div>
+            <div className={styles.actions}>
+              <img className={styles.likeIcon} alt="" src="/like3.svg" />
+            </div>
+            <div className={styles.reactions}>
+              <div className={styles.likes}>2 Likes</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.thread}>
+        <div className={styles.thread1}>
+          <div className={styles.thread2}>
+            <img className={styles.avatarIcon} alt="" src="/avatar5.svg" />
+          </div>
+          <div className={styles.infosActions}>
+            <div className={styles.info}>
+              <div className={styles.headingInfos}>
+                <div className={styles.aura}>aura</div>
+                <div className={styles.rightInfos}>
+                  <div className={styles.min}>2min</div>
+                  <div className={styles.dots}>
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                    <div className={styles.dotsChild} />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.iveBeenExploring}>
+                Hold onto your seats, folks! The new iPhone is coming in hot,
+                ready to rock our world! Imagine the stunning photos and videos
+                I'll capture with this beauty! 📸💥 #BringItOnNewiPhone
+                #BeyondExcited
+              </div>
+            </div>
+            <div className={styles.actions}>
+              <img className={styles.likeIcon} alt="" src="/like3.svg" />
+            </div>
+            <div className={styles.reactions1}>
+              <div className={styles.likes}>16 Likes</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/thread1.js
+++ components/thread1.js
@@ -1,17 +1,17 @@
-import styles from "./thread.module.css";
-const Thread = ({ likeCount, showReactions, threadContentText }) => {
+import styles from "./thread1.module.css";
+const Thread = ({ threadContentText, showLikes, username, timeSpent }) => {
   return (
     <div className={styles.thread}>
       <div className={styles.thread1}>
         <div className={styles.thread2}>
-          <img className={styles.avatarIcon} alt="" src="/avatar6.svg" />
+          <img className={styles.avatarIcon} alt="" src="/avatar.svg" />
         </div>
         <div className={styles.infosActions}>
           <div className={styles.info}>
             <div className={styles.headingInfos}>
-              <div className={styles.aura}>aura</div>
+              <div className={styles.aura}>{username}</div>
               <div className={styles.rightInfos}>
-                <div className={styles.min}>2min</div>
+                <div className={styles.min}>{timeSpent}</div>
                 <div className={styles.dots}>
                   <div className={styles.dotsChild} />
                   <div className={styles.dotsChild} />
@@ -19,16 +19,14 @@ const Thread = ({ likeCount, showReactions, threadContentText }) => {
                 </div>
               </div>
             </div>
-            <div className={styles.iveBeenExploring}>{threadContentText}</div>
+            <div className={styles.threadContentText}>{threadContentText}</div>
           </div>
           <div className={styles.actions}>
             <img className={styles.likeIcon} alt="" src="/like3.svg" />
           </div>
-          {showReactions && (
-            <div className={styles.reactions}>
-              <div className={styles.likes}>{likeCount}</div>
-            </div>
-          )}
+          <div className={styles.reactions}>
+            {showLikes && <div className={styles.likes}>7 Likes</div>}
+          </div>
         </div>
       </div>
     </div>
