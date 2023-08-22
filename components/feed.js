b7c6bb3b078d3633932a302f73fbd32c7a3f8bbd
Ignacio1996 <Ignacio1996>
2023-08-22 21:47:14 +0000

    Pushed from Locofy Builder

--- components/feed.js
+++ components/feed.js
@@ -6,53 +6,33 @@ const Feed = () => {
     <div className={styles.feed}>
       <ThreadInput />
       <Thread
-        componentCode="/like3.svg"
         likeCount="7 Likes"
         showReactions
         auraColor="#fff"
-        ellipseDivBackgroundColor="#fff"
-        ellipseDivBackgroundColor1="#fff"
-        ellipseDivBackgroundColor2="#fff"
         threadContentText="I’ve been exploring ways of setting up variables in Figma if you have two different sets of global colours for light and dark themes with multiple brands. If you want to learn more about it, DM me, please"
       />
       <Thread
-        componentCode="/like3.svg"
         likeCount="2 Likes"
         showReactions={false}
         auraColor="#fff"
-        ellipseDivBackgroundColor="#fff"
-        ellipseDivBackgroundColor1="#fff"
-        ellipseDivBackgroundColor2="#fff"
         threadContentText={`guys, just see that now we have a "Save for Later" I've been waiting for this so much`}
       />
       <Thread
-        componentCode="/like3.svg"
         likeCount="16 Likes"
         showReactions
         auraColor="#fff"
-        ellipseDivBackgroundColor="#fff"
-        ellipseDivBackgroundColor1="#fff"
-        ellipseDivBackgroundColor2="#fff"
         threadContentText="Hold onto your seats, folks! The new iPhone is coming in hot, ready to rock our world! Imagine the stunning photos and videos I'll capture with this beauty! 📸💥 #BringItOnNewiPhone #BeyondExcited"
       />
       <Thread
-        componentCode="/like3.svg"
         likeCount="2 Likes"
         showReactions={false}
         auraColor="#fff"
-        ellipseDivBackgroundColor="#fff"
-        ellipseDivBackgroundColor1="#fff"
-        ellipseDivBackgroundColor2="#fff"
         threadContentText={`guys, just see that now we have a "Save for Later" I've been waiting for this so much`}
       />
       <Thread
-        componentCode="/like3.svg"
         likeCount="16 Likes"
         showReactions
         auraColor="#fff"
-        ellipseDivBackgroundColor="#fff"
-        ellipseDivBackgroundColor1="#fff"
-        ellipseDivBackgroundColor2="#fff"
         threadContentText="Hold onto your seats, folks! The new iPhone is coming in hot, ready to rock our world! Imagine the stunning photos and videos I'll capture with this beauty! 📸💥 #BringItOnNewiPhone #BeyondExcited"
       />
     </div>
--- components/thread.js
+++ components/thread.js
@@ -1,38 +1,11 @@
 import { useMemo } from "react";
 import styles from "./thread.module.css";
-const Thread = ({
-  componentCode,
-  likeCount,
-  showReactions,
-  auraColor,
-  ellipseDivBackgroundColor,
-  ellipseDivBackgroundColor1,
-  ellipseDivBackgroundColor2,
-  threadContentText,
-}) => {
+const Thread = ({ likeCount, showReactions, auraColor, threadContentText }) => {
   const auraStyle = useMemo(() => {
     return {
       color: auraColor,
     };
   }, [auraColor]);
-
-  const ellipseDivStyle = useMemo(() => {
-    return {
-      backgroundColor: ellipseDivBackgroundColor,
-    };
-  }, [ellipseDivBackgroundColor]);
-
-  const ellipseDiv1Style = useMemo(() => {
-    return {
-      backgroundColor: ellipseDivBackgroundColor1,
-    };
-  }, [ellipseDivBackgroundColor1]);
-
-  const ellipseDiv2Style = useMemo(() => {
-    return {
-      backgroundColor: ellipseDivBackgroundColor2,
-    };
-  }, [ellipseDivBackgroundColor2]);
 
   return (
     <div className={styles.thread}>
@@ -49,16 +22,16 @@ const Thread = ({
               <div className={styles.rightInfos}>
                 <div className={styles.min}>2min</div>
                 <div className={styles.dots}>
-                  <div className={styles.dotsChild} style={ellipseDivStyle} />
-                  <div className={styles.dotsChild} style={ellipseDiv1Style} />
-                  <div className={styles.dotsChild} style={ellipseDiv2Style} />
+                  <div className={styles.dotsChild} />
+                  <div className={styles.dotsChild} />
+                  <div className={styles.dotsChild} />
                 </div>
               </div>
             </div>
             <div className={styles.iveBeenExploring}>{threadContentText}</div>
           </div>
           <div className={styles.actions}>
-            <img className={styles.likeIcon} alt="" src={componentCode} />
+            <img className={styles.likeIcon} alt="" src="/like3.svg" />
           </div>
           {showReactions && (
             <div className={styles.reactions}>
