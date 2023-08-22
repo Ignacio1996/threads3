34198e7f4e11f0ec87e5edb83552a3c25c1f7a0d
Ignacio1996 <Ignacio1996>
2023-08-22 19:09:56 +0000

    Pushed from Locofy Builder

--- components/ThreadsColMid/avatar-big-icon.js
+++
@@ -1,6 +0,0 @@
-import styles from "./avatar-big-icon.module.css";
-const AvatarBig = ({ imageDescription }) => {
-  return <img className={styles.avatarBigIcon} alt="" src={imageDescription} />;
-};
-
-export default AvatarBig;
