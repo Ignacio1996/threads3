f2109cf082f21256638d9934bfa2272b0c8ee9b0
Ignacio1996 <Ignacio1996>
2023-08-23 15:11:01 +0000

    Pushed from Locofy Builder

--- components/thread2.js
+++ components/thread2.js
@@ -1,17 +1,17 @@
 import styles from "./thread1.module.css";
-const Thread = ({ threadContentText, showLikes, username, timeSpent }) => {
+const Thread = ({ avatar, threadContentText, likes, showReactions }) => {
   return (
     <div className={styles.thread}>
       <div className={styles.thread1}>
         <div className={styles.thread2}>
-          <img className={styles.avatarIcon} alt="" src="/avatar.svg" />
+          <img className={styles.avatarIcon} alt="" src={avatar} />
         </div>
         <div className={styles.infosActions}>
           <div className={styles.info}>
             <div className={styles.headingInfos}>
-              <div className={styles.aura}>{username}</div>
+              <div className={styles.username}>aura</div>
               <div className={styles.rightInfos}>
-                <div className={styles.min}>{timeSpent}</div>
+                <div className={styles.min}>2min</div>
                 <div className={styles.dots}>
                   <div className={styles.dotsChild} />
                   <div className={styles.dotsChild} />
@@ -24,9 +24,11 @@ const Thread = ({ threadContentText, showLikes, username, timeSpent }) => {
           <div className={styles.actions}>
             <img className={styles.likeIcon} alt="" src="/like3.svg" />
           </div>
-          <div className={styles.reactions}>
-            {showLikes && <div className={styles.likes}>7 Likes</div>}
-          </div>
+          {showReactions && (
+            <div className={styles.reactions}>
+              <div className={styles.likes}>{likes}</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
